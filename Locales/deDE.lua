b987e61aeab96912131015feddefde0e0a9dbc41
nebula169 <nebula169@d3eeec64-f9fa-4462-9b7c-4f668c8f0482>
2010-12-29 18:37:27 +0000

    - return early on unused locales

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -7,6 +7,7 @@ Translated by:
 ]]
 
 local L = LibStub("AceLocale-3.0"):NewLocale("TankPoints", "deDE")
+if not L then return end
 
 -- To translate AceLocale strings, replace true with the translation string
 -- Before: ["Show Item ID"] = true
--- Locales/zhTW.lua
+++ Locales/zhTW.lua
@@ -9,6 +9,7 @@ Translated by:
 ]]
 
 local L = LibStub("AceLocale-3.0"):NewLocale("TankPoints", "zhTW")
+if not L then return end
 
 -- To translate AceLocale strings, replace true with the translation string
 -- Before: ["Show Item ID"] = true
